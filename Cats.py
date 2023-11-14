b8374d880b63663981aa1d9567673246a9b0fe43
Moralex69 <morro69@mail.ru>
2023-11-14 12:47:41 +0300

    new file

---
+++ Cats.py
@@ -0,0 +1,5 @@
+from  cat import Cat
+Pet1 = Cat('Барон', 'мальчик', '2')
+Pet2 = Cat('Сам', 'мальчик', '2')
+print(f"\n Имя: {Pet1.name},\n Пол: {Pet1.gender},\n Возраст: {Pet1.age}")
+print(f"\n Имя: {Pet2.name},\n Пол: {Pet2.gender},\n Возраст: {Pet2.age}")
